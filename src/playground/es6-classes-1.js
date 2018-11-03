6fe06fd64864cdcb45640849b6a807b794eb5991
Adam Pluth <adam.pluth@gmail.com>
2018-11-03 17:39:42 -0600

    added es6 classes part 1

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,12 +1,21 @@
 
 class Person {
-    constructor(name = 'Anonymous') {
+    constructor(name = 'Anonymous', age = 0) {
         this.name = name;
+        this.age = age;
+    }
+    getGreeting() {
+        return `Hi, I am ${ this.name }!`;
+    }
+    getDescription() {
+        return `${ this.name } is ${ this.age } year(s) old.`;
     }
 }
 
-const me = new Person('Adam Pluth');
-console.log(me);
+const me = new Person('Adam Pluth', 33);
+console.log(me.getGreeting());
+console.log(me.getDescription());
 
 const other = new Person();
-console.log(other);
+console.log(other.getGreeting());
+console.log(other.getDescription());
